892e295620d0b9d0c00782d9c558f86fdc51a499
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-12 01:25:54 +0700

    Added ConditionBlock with processing EndBlock

--- app/src/main/java/com/example/android_7_module_hits/interpreter/interpret.kt
+++ app/src/main/java/com/example/android_7_module_hits/interpreter/interpret.kt
@@ -2,30 +2,47 @@ package com.example.android_7_module_hits.interpreter
 
 import com.example.android_7_module_hits.Blocks.Block
 import com.example.android_7_module_hits.Blocks.BlockContent
+import com.example.android_7_module_hits.Blocks.BlockType
 
 fun interpret(block: Block, state: InterpreterState) {
-    when (val content = block.content) {
+    var currentBlock = block
+
+    when (val content = currentBlock.content) {
         is BlockContent.Declare -> {
             val variableName = content.name?.trim() ?: "unnamed"
             state.declareVariable(variableName)
         }
+
         is BlockContent.Assignment -> {
             val variableName = content.name?.trim() ?: "unnamed"
             val expr = content.value?.trim() ?: "0"
-
             state.assignValue(variableName, expr)
         }
+
         is BlockContent.Condition -> {
             val firstExpr = content.firstPart?.trim() ?: "0"
             val operate = content.operator?.trim() ?: "=="
             val secondExpr = content.secondPart?.trim() ?: "0"
 
-            state.setCondition(firstExpr, operate, secondExpr)
+            val conditionResult = state.setCondition(firstExpr, operate, secondExpr)
+
+            var current: Block? = currentBlock.child
+            var insideIfBody = conditionResult
+
+            while (current != null && current.content !is BlockContent.End) {
+                if (insideIfBody) {
+                    interpret(current, state)
+                }
+                current = current.child
+            }
+
+
+            current?.child?.let { interpret(it, state) }
+            return
         }
+
         is BlockContent.End -> {}
     }
 
-    block.child?.let { child ->
-        interpret(child, state)
-    }
+    currentBlock.child?.let { interpret(it, state) }
 }
