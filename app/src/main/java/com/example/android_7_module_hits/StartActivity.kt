65e0d9213caa312c4d11d7b31ee74333b0da0494
Maksimcode <max.shevtsov1@gmail.com>
2025-05-11 11:03:08 +0700

    Add splashscreen + delete first screen with choice "codeblock & Library". Now the programm starts in Library right after custom splashscreen

--- app/src/main/java/com/example/android_7_module_hits/StartActivity.kt
+++ app/src/main/java/com/example/android_7_module_hits/StartActivity.kt
@@ -4,59 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.android_7_module_hits.ui.theme.Android7ModuleHITsTheme
 
 class StartActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(1500)
+        installSplashScreen()
         setContent {
             Android7ModuleHITsTheme {
-                Box(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Column(
-                        modifier = Modifier.background(Color.Gray)
-                            .align(Alignment.BottomCenter)
-                    ) {
-                        Button(
-                            onClick = {
-                                val intent = Intent(this@StartActivity, MainActivity::class.java)
-                                startActivity(intent)
-                            },
-                            modifier = Modifier
-                                .padding(5.dp)
-                                .fillMaxWidth(),
-                            shape = RoundedCornerShape(10.dp)
-                        ) {
-                            Text(text = "To codeblock")
-                        }
-
-
-                        Button(onClick = {
-                            val intent = Intent(this@StartActivity, LibraryActivity::class.java)
-                            startActivity(intent)
-                        },
-                        modifier = Modifier
-                            .padding(5.dp)
-                            .fillMaxWidth(),
-                        shape = RoundedCornerShape(10.dp)
-                    ) {
-                        Text(text = "To library")
-                    }
-                  }
-                }
+                val intent = Intent(this, LibraryActivity::class.java)
+                startActivity(intent)
+                finish()
             }
         }
     }
