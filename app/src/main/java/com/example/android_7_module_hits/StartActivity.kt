b5abeb23dffd4f6cd038fbf776b440e8c3698e6c
Maksimcode <max.shevtsov1@gmail.com>
2025-05-11 14:21:11 +0700

    Add navigation instead many component activities

--- app/src/main/java/com/example/android_7_module_hits/StartActivity.kt
+++ app/src/main/java/com/example/android_7_module_hits/StartActivity.kt
@@ -1,22 +1,25 @@
 package com.example.android_7_module_hits
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.example.android_7_module_hits.ui.theme.Android7ModuleHITsTheme
 
 class StartActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(1500)
         installSplashScreen()
         setContent {
             Android7ModuleHITsTheme {
-                val intent = Intent(this, LibraryActivity::class.java)
-                startActivity(intent)
-                finish()
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
