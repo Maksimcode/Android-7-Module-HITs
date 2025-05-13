8e62b3b72dec7c617dce9daebd3e0a7d5bf6fe93
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-13 23:54:06 +0700

    update singleton

--- app/src/main/java/com/example/android_7_module_hits/Blocks/BlockManager.kt
+++ app/src/main/java/com/example/android_7_module_hits/Blocks/BlockManager.kt
@@ -1,10 +1,12 @@
 package com.example.android_7_module_hits.Blocks
 
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 
+// В BlockManager:
 object BlockManager {
     private val _allBlocks = mutableStateListOf<Block>()
-    val allBlocks: List<Block> = _allBlocks
+    val allBlocks: SnapshotStateList<Block> = _allBlocks
 
     fun addBlock(block: Block) {
         _allBlocks.add(block)
@@ -14,8 +16,18 @@ object BlockManager {
         _allBlocks.remove(block)
     }
 
-    fun updateBlocks(newList: List<Block>) {
+    fun updateBlock(updatedBlock: Block) {
+        val index = _allBlocks.indexOfFirst { it.id == updatedBlock.id }
+        if (index != -1) {
+            _allBlocks[index] = updatedBlock
+        }
+    }
+
+    fun updateAllBlocks(newList: List<Block>) {
         _allBlocks.clear()
         _allBlocks.addAll(newList)
     }
 }
+
+
+
