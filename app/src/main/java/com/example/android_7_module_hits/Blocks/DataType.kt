a6934aee62fb757ceb7722ea67a29d06d3095e07
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-16 23:17:52 +0700

    Added enum class DataType

---
+++ app/src/main/java/com/example/android_7_module_hits/Blocks/DataType.kt
@@ -0,0 +1,6 @@
+package com.example.android_7_module_hits.Blocks
+
+enum class DataType {
+    INTEGER, BOOLEAN, DOUBLE, FLOAT, STRING, CHAR,
+    ARR_INT, ARR_BOOL, ARR_DOUBLE, ARR_FLOAT, ARR_STR, ARR_CHAR
+}
