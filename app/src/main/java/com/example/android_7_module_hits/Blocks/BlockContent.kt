ad48e62adea1c894cd4e61386f0ecdfe1b01d8aa
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-17 00:58:37 +0700

    Some changes in BlockContent

--- app/src/main/java/com/example/android_7_module_hits/Blocks/BlockContent.kt
+++ app/src/main/java/com/example/android_7_module_hits/Blocks/BlockContent.kt
@@ -3,14 +3,14 @@ package com.example.android_7_module_hits.Blocks
 sealed class BlockContent{
 
     data class Declare(
-        var type: DataType,
+        var type: DataType?,
         var name: String,
         val value: String = "0"
     ) :BlockContent()
 
     data class Assignment(
         var name: String,
-        var value: String
+        var value: String?
     ) :BlockContent()
 
     data class Condition(
