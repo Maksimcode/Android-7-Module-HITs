f8b876e14855a6061bc69cf1dd79604db62ebcfa
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-16 23:18:35 +0700

    Updated Content and DeclationBlock for using DataType

--- app/src/main/java/com/example/android_7_module_hits/Blocks/DeclarationBlock.kt
+++ app/src/main/java/com/example/android_7_module_hits/Blocks/DeclarationBlock.kt
@@ -1,11 +1,13 @@
 package com.example.android_7_module_hits.Blocks
 
 class DeclarationBlock(
+    val variableType: DataType,
     val variableName: String,
     val initialValue: String? = "0"
 ) : BaseBlock(
     type = BlockType.DECLARE,
     content = BlockContent.Declare(
+        type = variableType,
         name = variableName
     )
 )
