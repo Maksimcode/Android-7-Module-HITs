22c426dfd59987b56a9184c1cb112ba96d810f9b
Zhuchkova Alzhbeta <zhuchkova.eliser@stud.tsu.ru>
2025-05-28 21:50:03 +0700

    added processing of forblock to interpreter

--- app/src/main/java/com/example/android_7_module_hits/blocks/ForBlock.kt
+++ app/src/main/java/com/example/android_7_module_hits/blocks/ForBlock.kt
@@ -1,15 +1,15 @@
 package com.example.android_7_module_hits.blocks
 
 class ForBlock (
-    val varCount: String,
-    val startPoint: String,
+    val counter: String,
+    val startValue: String,
     val logicalExpression: String,
     val update: String
 ) : BlockHasBody(
     type = BlockType.FOR,
     content = BlockContent.For(
-        variable = varCount,
-        initValue = startPoint,
+        variable = counter,
+        initValue = startValue,
         expression = logicalExpression,
         construct = update
     )
